5763edbd912bd1b827ef05540611f58df7eb10a6
MrHavoc <official.lurnaihub@gmail.com>
2026-07-07 19:54:50 -0500

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
   [994732206] = "", -- Blox Fruit
-  [1268927906] = "", -- Muscle Legends
+  [1268927906] = "https://api.jnkie.com/api/v1/luascripts/public/435c6af5fbde9385ae6066c8dfd9639fc6c8a2067beadf601d27b43b934ea669/download", -- Muscle Legends
 }
 return Games
