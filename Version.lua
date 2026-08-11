08ab861b537e6aaf5f536d105b533072abf45bcf
MrHavoc <official.lurnaihub@gmail.com>
2026-08-10 21:48:35 -0500

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -2,7 +2,7 @@ return {
     LoaderVersion = "1.0.0",
     Games = {
         [994732206] = "1.0.0", -- Blox Fruits
-        [1268927906] = "1.1.2", -- Muscle Legends
+        [3623096087] = "1.1.2", -- Muscle Legends
         [142823291] = "1.0.0", -- MM2
     }
 }
